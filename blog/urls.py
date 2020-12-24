1fc267b6df06cae0762e5fc84496f74d55a97103
ratanmamdekar <ratanmamdekar@gmail.com>
2020-12-24 16:14:55 +0530

    added welcome page and extended templete

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('home', views.post_list, name='post_list'),
+    path('page2', views.post_list, name='post_list'),
+    path('welcome', views.welcome, name='welcome'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -7,3 +7,8 @@ def post_list(request):
     posts = Post.objects.filter(
         published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def welcome(request):
+
+    return render(request, 'blog/welcome.html')
