86b4c7eb95aead82bd3d911d0f6dffbb253e7ab4
ratanmamdekar <ratanmamdekar@gmail.com>
2020-12-24 16:21:34 +0530

    modified welcome page

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('home', views.post_list, name='post_list'),
     path('page2', views.post_list, name='post_list'),
+    path('', views.welcome, name='welcome'),
     path('welcome', views.welcome, name='welcome'),
+
 ]
