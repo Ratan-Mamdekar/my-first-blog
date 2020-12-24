11d005bf3cfe0cf53b05c8de785e3ba202a46ab7
ratanmamdekar <ratanmamdekar@gmail.com>
2020-12-24 21:29:22 +0530

    with django forms for new posts

--- blog/urls.py
+++ blog/urls.py
@@ -3,9 +3,8 @@ from . import views
 
 urlpatterns = [
     path('home', views.post_list, name='post_list'),
-    path('page2', views.post_list, name='post_list'),
     path('', views.welcome, name='welcome'),
     path('welcome', views.welcome, name='welcome'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
-
+    path('post/new/', views.post_new, name='post_new'),
 ]
